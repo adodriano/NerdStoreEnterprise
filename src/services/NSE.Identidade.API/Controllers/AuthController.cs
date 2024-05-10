6fac0da893d260054f17bb9a6d0d7cd387f97923
adodriano <adodriano@gmail.com>
2024-05-10 17:32:52 -0300

    Add project files.

---
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _UserManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _UserManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro registro)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = registro.Email,
+                Email = registro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _UserManager.CreateAsync(user, registro.Senha);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin login)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(login.Email, login.Senha, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+
+        }
+    }
+}
---
+++ src/services/NSE.Identidade.API/Models/UserViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identidade.API.Models
+{
+    public class UsuarioRegistro
+    {
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O Campo {0} inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string SenhaConfirmacao { get; set; }
+    }
+
+    public class UsuarioLogin
+    {
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O Campo {0} inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O Campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+
+    }
+}
